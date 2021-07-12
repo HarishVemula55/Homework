8b35b7f7bee8cfdecc8128482d6f6840f97db3af
Ramesh Reddy <rameshreddy@Rameshs-MBP.hsd1.ca.comcast.net>
2021-07-12 13:49:34 -0700

    Completed rewards app

---
+++ src/data/Data.js
@@ -0,0 +1,47 @@
+export const DATASET = [
+    {id : 1, name : "John", amount : 110, transactionId : 1, transactionDate : new Date("01/07/2020")},
+    {id : 1, name : "John", amount : 52, transactionId : 2, transactionDate : new Date("01/10/2020")},
+    {id : 1, name : "John", amount : 140, transactionId : 3, transactionDate : new Date("01/21/2020")},
+    {id : 1, name : "John", amount : 21, transactionId : 4, transactionDate : new Date("01/31/2020")},
+
+    {id : 1, name : "John", amount : 130, transactionId : 5, transactionDate : new Date("02/07/2020")},
+    {id : 1, name : "John", amount : 520, transactionId : 6, transactionDate : new Date("02/10/2020")},
+    {id : 1, name : "John", amount : 140, transactionId : 7, transactionDate : new Date("02/21/2020")},
+    {id : 1, name : "John", amount : 21, transactionId : 8, transactionDate : new Date("02/23/2020")},
+
+    {id : 1, name : "John", amount : 10, transactionId : 9, transactionDate : new Date("03/07/2020")},
+    {id : 1, name : "John", amount : 52, transactionId : 10, transactionDate : new Date("03/10/2020")},
+    {id : 1, name : "John", amount : 140, transactionId : 11, transactionDate : new Date("03/21/2020")},
+    {id : 1, name : "John", amount : 21, transactionId : 12, transactionDate : new Date("03/31/2020")},
+
+    {id : 2, name : "Carry", amount : 210, transactionId : 13, transactionDate : new Date("01/07/2020")},
+    {id : 2, name : "Carry", amount : 112, transactionId : 14, transactionDate : new Date("01/10/2020")},
+    {id : 2, name : "Carry", amount : 140, transactionId : 15, transactionDate : new Date("01/21/2020")},
+    {id : 2, name : "Carry", amount : 21, transactionId : 16, transactionDate : new Date("01/31/2020")},
+
+    {id : 2, name : "Carry", amount : 110, transactionId : 17, transactionDate : new Date("02/07/2020")},
+    {id : 2, name : "Carry", amount : 12, transactionId : 18, transactionDate : new Date("02/10/2020")},
+    {id : 2, name : "Carry", amount : 140, transactionId : 19, transactionDate : new Date("02/21/2020")},
+    {id : 2, name : "Carry", amount : 21, transactionId : 20, transactionDate : new Date("02/24/2020")},
+
+    {id : 2, name : "Carry", amount : 210, transactionId : 21, transactionDate : new Date("03/07/2020")},
+    {id : 2, name : "Carry", amount : 42, transactionId : 22, transactionDate : new Date("03/10/2020")},
+    {id : 2, name : "Carry", amount : 140, transactionId : 23, transactionDate : new Date("03/21/2020")},
+    {id : 2, name : "Carry", amount : 61, transactionId : 24, transactionDate : new Date("03/31/2020")},
+
+    {id : 3, name : "Smith", amount : 180, transactionId : 25, transactionDate : new Date("01/07/2020")},
+    {id : 3, name : "Smith", amount : 210, transactionId : 26, transactionDate : new Date("01/10/2020")},
+    {id : 3, name : "Smith", amount : 140, transactionId : 27, transactionDate : new Date("01/21/2020")},
+    {id : 3, name : "Smith", amount : 21, transactionId : 28, transactionDate : new Date("01/31/2020")},
+
+    {id : 3, name : "Smith", amount : 110, transactionId : 29, transactionDate : new Date("02/07/2020")},
+    {id : 3, name : "Smith", amount : 52, transactionId : 30, transactionDate : new Date("02/10/2020")},
+    {id : 3, name : "Smith", amount : 140, transactionId : 31, transactionDate : new Date("02/21/2020")},
+    {id : 3, name : "Smith", amount : 111, transactionId : 32, transactionDate : new Date("02/25/2020")},
+
+    {id : 3, name : "Smith", amount : 118, transactionId : 33, transactionDate : new Date("03/07/2020")},
+    {id : 3, name : "Smith", amount : 67, transactionId : 34, transactionDate : new Date("03/10/2020")},
+    {id : 3, name : "Smith", amount : 120, transactionId : 35, transactionDate : new Date("03/21/2020")},
+    {id : 3, name : "Smith", amount : 216, transactionId : 36, transactionDate : new Date("03/31/2020")},
+
+];
